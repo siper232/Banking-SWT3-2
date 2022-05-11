f872527572a1cadb20312c126f134a727efc721e
Mathieu Bosse <mathieu2011@live.nl>
2022-05-11 21:16:45 +0200

    added and tested most features
    Just need to add the locking after 12 months and the simulation with the interests.

--- Banking.py
+++ Banking.py
@@ -1,97 +1,140 @@
-# clientInfo = {
-#   Max: {
-#     balance: 5000,
-#     loan: 200,
-#     currentInterest: 50,
-#   },
-#   John: {
-#     balance: 2000,
-#     loan: 0,
-#     currentInterest: 0,
-#   },
-#   Nobody: {
-#     balance: 6000,
-#     loan: 100,
-#     currentInterest: 70,
-#   },
-# }
+maxLoanAmount = 10000.00
+maxLoans = 3
+minBalance = 50.00
 
-class Accounts:
-  def __init__(self, name, balance, loan):
+interestRate = 0.05  # 5%
+startInterestLoan = 0.1  # 10%
+maxAdaptation = 0.005  # 0.5%
+loanMaxMonths = 12
+currentLoanInterest = startInterestLoan
+
+totalMoney = 0
+
+homeLogin = ["help", "login", "signup", "exit"]
+loginCommands = ["help", "home", "signup", "exit"]
+signupCommands = ["help", "home", "login", "exit"]
+loggedInCommands = ["help", "logout", "checkAccount", "createLoan",
+    "makeDeposit", "makeLoanDeposit", "makeWithdrawal", "exit"]
+transactionCommands = ["help", "cancel"]
+
+clientsInfo = []
+
+
+class Account:
+  def __init__(self, name, balance):
     self.name = name
-    self.balance = balance
-    self.loan = loan
+    self.balance = float(balance)
+    self.loans = []
+    self.locked = False
 
-noRightsCommands = ["help", "login", "signup"]
-normalUserRights = ["help", "logout", "exit"]
-adminUserRIghts = ["help", "logout", "exit"]
+  def makeDeposit(self, deposit):
+    self.balance += float(deposit)
 
-interest = 0.1
+  def makeWithdrawal(self, withdraw):
+    self.balance -= float(withdraw)
 
-command = ""
+  def makeLoanDeposit(self, idx, deposit):
+    self.loans[idx].makeDeposit(deposit)
+    self.balance = float(self.balance - deposit)
+    if self.loans[idx].amount == 0:
+      self.loans.pop(idx)
+
+  def addMonth(self):
+    for loan in self.loans:
+      loan.addMonth
+
+
+class Loan:
+  def __init__(self, amount):
+    self.amount = float(amount)
+    self.time = 0  # in months
+
+  def makeDeposit(self, deposit):
+    self.amount -= float(deposit)
+
+  def addMonth(self):
+    self.time += 1
+    self.amount = float(self.amount * currentLoanInterest)
+
+
+clientsInfo.append(Account("siper", 4000.00))
+
+clientsInfo[0].loans.append(Loan(200.00))
+
 user = ""
+userIdx = None
 
 
 def welcome():
-  succeeded = False
+  global user
 
-  print("Welcome to the bank of fuck you, we give no fucks :)\n\n")
-  print("For a list of all commands at any point type \"help\"\n")
+  print("\n\n\nWelcome to the bank of fuck you, we give no fucks :)\n\n")
+  print("For a list of all commands at any point type \"help\".\n")
 
-  while not succeeded:
-    command = input("Please choose whether to \"login\" or to \"signup\"\n")
+  while True:
+    command = input("Please choose whether to \"login\" or to \"signup\".\n")
     if command == "help":
-      print("Possible commands:\n\tlogin\n\tsignup\n\n")
+      possibleCommands(homeLogin)
     elif command == "exit":
-      quit()
-    elif command != "login" and command != "signup":
-      print("Command was not accepted, please try again\n")
+      exit()
     elif command == "login":
-      succeeded = True
       login()
     elif command == "signup":
       signup()
     else:
-      print("Unknown error, please try again\n")
+      print("Command was not accepted, please try again.\n")
 
 
 def login():
-  succeeded = False
+  global user
+  global userIdx
 
-  print("Welcome to the fuck you bank login\n")
+  succeeded = False
+  clientNames = [client.name for client in clientsInfo]
+
+  print("\n\nWelcome to fuck you bank login.\n")
 
   while not succeeded:
-    command = input("Please provide your username\n")
+    command = input("Please provide your username to log in.\n")
 
     if command == "help":
-      print("Possible commands:\n\t...\n")
+      possibleCommands(loginCommands)
     elif command == "exit":
-      quit()
+      exit()
     elif command == "home":
-      print("returning to home\n\n")
+      print("Returning to home.\n\n")
       welcome()
-    elif clientInfo.has_key(command):
+    elif command in clientNames:
       user = command
+      userIdx = clientNames.index(command)
+      succeeded = True
     else:
-      print("Username/command does not exist, please try again or type \"exit\" to exit")
+      print("Username/command does not exist, please try again or type \"help\" for possible commands.\n")
+  loggedIn()
 
 
 def signup():
+  global user
+
   usernameSucceeded = False
   accountSucceeded = False
+  clientNames = [client.name for client in clientsInfo]
 
-  print("Welcome to fuck you bank signup\n")
+  print("\n\nWelcome to fuck you bank signup\n")
 
   while not usernameSucceeded:
     command = input("Please insert your username\n")
     if command == "help":
-      print("Possible commands:\n\t...\n")
+      possibleCommands(signupCommands)
     elif command == "exit":
-      quit()
+      exit()
+    elif command == "home":
+      print("Returning to home.\n\n")
+      welcome()
     elif " " in command:
-      print("Username can only be one word\n")
-    elif clientInfo.has_key(command):
-      print("Username already exists, please use another one\n")
+      print("Username can only be one word.\n")
+    elif command in clientNames:
+      print("Username already exists, please use another one.\n")
     else:
       usernameSucceeded = True
       user = command
@@ -99,34 +142,236 @@ def signup():
   while not accountSucceeded:
     command = input("Please insert your balance\n")
     if command == "help":
-      print("Possible commands:\n\t...\n")
-    elif not command.isnumeric():
-      print("Balance can only consist of numbers\n")
+      possibleCommands(signupCommands)
+    elif command == "exit":
+      exit()
+    elif command == "home":
+      print("returning to home\n\n")
+      welcome()
     else:
-      accountSucceeded = True
-      # clientInfo[user] = {
-      #   balance = int(command),
-      #   interest: 0,
-      # }
+      if correctMoney(command):
+        if float(command) < minBalance:
+          print("Account cannot be created with a balance below " +
+                str(minBalance) + " euros.\nPlease try again\n")
+        else:
+          makeAccount(user, float(command))
+          accountSucceeded = True 
+
+  print("You have successfully created your account. You will be redirected to the home page.\n")
+  welcome()
 
 
 def loggedIn():
-  print("Welcome " + user + " to the fuck you bank.\n\n")
-  command = input("How can we help you today?")
-  if command == "help":
-    print("Possible commands:\n\t...\n")
-  elif command == "exit":
-    quit()
+  global user
+  global userIdx
+
+  online = True
+
+  print("\n\nWelcome " + user + ", to the fuck you bank.\n\n")
+  while online:
+    command = input("How can we help you today?\n")
+    if command == "help":
+      possibleCommands(loggedInCommands)
+    elif command == "exit":
+      exit()
+    elif command == "logout":
+      user = ""
+      userIdx = None
+      online = False
+      welcome()
+    elif command == "checkAccount":
+      showAccount()
+    elif command == "createLoan":
+      newLoan()
+    elif command == "makeDeposit":
+      makeDeposit()
+    elif command == "makeLoanDeposit":
+      makeLoanDeposit()
+    elif command == "makeWithdrawal":
+      makeWithdrawal()
+    else:
+      print("Command does not exist, please try again or type \"help\" for possible commands.\n")
+
+
+def newLoan():
+  global clientsInfo
+
+  completed = False
+
+  if len(clientsInfo[userIdx].loans) == maxLoans:
+    print("You can not get a new loan. You already have " + str(maxLoans) + " loans.")
   else:
-    print("Something went wrong, please try again")
+    while not completed:
+      command = input("How much money would you like to lend?\n")
+      if command == "help":
+        possibleCommands(transactionCommands)
+      elif command == "cancel":
+        completed = True
+      elif command == "exit":
+        exit()
+      else:
+        if correctMoney(command):
+          if float(command) < maxLoanAmount:
+            newLoan = Loan(float(command))
+            clientsInfo[userIdx].loans.append(newLoan)
+            print("Your new loan has been created with an amount of " + str(float(command)) + " euros\n")
+            completed = True
+          else:
+            print("The max amount of a loan is " + str(maxLoanAmount) + " euros. Please input a number below this amount.")
 
 
-# while user == "":
-#     command = input("Please provide your name to sign in.\n")
-#     if (command in clientInfo):
-#         user = command
-#         print("Welcome " + user + " to the bank of fuck you")
-#     elif command == "exit":
-#         print("Exiting login\n\n")
-#     else:
-#         print("Username does not exist, please try again or type \"exit\" to exit")
+def makeDeposit():
+  global totalMoney
+
+  completed = False
+  
+  while not completed:
+    command = input("How much money would you like to put into your account?\n")
+    if command == "help":
+      possibleCommands(transactionCommands)
+    elif command == "cancel":
+      completed = True
+    elif command == "exit":
+      exit()
+    else:
+      if correctMoney(command):
+        clientsInfo[userIdx].makeDeposit(command)
+        print("You have successfully deposited " + str(float(command)) + " euros\n")
+        totalMoney += float(command)
+        completed = True
+
+
+def makeLoanDeposit():
+  completed = False
+  completed2 = False
+
+  showAccount()
+
+  while not completed:
+    command = input("What loan would you like to make a deposit on? (look at the number before the loans, example: 1)\n")
+    if command == "help":
+      possibleCommands(transactionCommands)
+    elif command == "cancel":
+      completed = True
+    elif command == "exit":
+      exit()
+    else:
+      try:
+        index = int(command)
+        if index <= len(clientsInfo[userIdx].loans) and index > 0:
+          loanIdx = index - 1
+          completed = True
+          print("")
+
+          while not completed2:
+            command = input("How much money would you like to deposit from your account to the selected loan?\n")
+            if command == "help":
+              possibleCommands(transactionCommands)
+            elif command == "cancel":
+              completed2 = True
+            elif command == "exit":
+              exit()
+            else:
+              if correctMoney(command):
+                if float(command) <= clientsInfo[userIdx].balance:
+                  if float(command) <= clientsInfo[userIdx].loans[loanIdx].amount:
+                    clientsInfo[userIdx].makeLoanDeposit(loanIdx, float(command))
+                    print("You have successfully deposited " + str(float(command)) + " euro to loan number " + str(index) + "\n")
+                    completed2 = True
+                  else:
+                    print("You can't deposit more money than needed to pay off your loan.\n")
+                else:
+                  print("You don't have enough money in your balance to pay this amount.\n")
+        else:
+          print("This loan does not exist, please try again.\n")
+      except ValueError:
+        print("Please provide a full number like 1, 2 or 3 to select your loan.\n")
+
+
+def makeWithdrawal():
+  global totalMoney
+
+  completed = False
+
+  while not completed:
+    command = input("How much money would you like to retrieve from your account?\n")
+    if command == "help":
+      possibleCommands(transactionCommands)
+    elif command == "cancel":
+      completed = True
+    elif command == "exit":
+      exit()
+    else:
+      if correctMoney(command):
+        if (clientsInfo[userIdx].balance >= float(command)):
+          clientsInfo[userIdx].makeWithdrawal(command)
+          print("You have successfully withdrawn " + str(float(command)) + " euros\n")
+          totalMoney -= float(command)
+          completed = True
+        else:
+          print("You do not have the balance to withdraw this amount of money, please input a different amount or type \"cancel\"")
+
+
+def showAccount():
+  client = clientsInfo[userIdx]
+  print("\nName: " + client.name + "\nBalance: " + str(client.balance) + "\nLoans:")
+  print("\tAmount\t\tCurrent interest\tLoan next month\t\tMonths to pay off")
+  for idx, loan in enumerate(client.loans):
+    print(str(idx + 1) + "\t" + str(loan.amount) + " euro\t" + str(currentLoanInterest*100) + "%\t\t\t" + str(loan.amount + (loan.amount * currentLoanInterest)) + " euro\t\t" + str(loanMaxMonths - loan.time) + " month(s)")
+  print("")
+
+
+def makeAccount(username, balance):
+  global totalMoney
+
+  newClient = Account(username, balance)
+  clientsInfo.append(newClient)
+  totalMoney += balance
+
+
+def possibleCommands(commands):
+  print("Possible commands:")
+  for command in commands:
+    print("\t" + command)
+  print("")
+
+
+def exit():
+  print("\n\nExitting program, have a nice day :)")
+  quit()
+
+def correctMoney(amount):
+  try:
+    money = int(amount)
+    money = float(amount)
+
+    return True
+
+  except ValueError:
+
+    try:
+      values = amount.split(".")
+
+      if len(values) == 2:
+        euros = int(values[0])
+        cents = int(values[1])
+
+        if len(values[1]) <= 2:
+
+          money = float(amount)
+
+          return True
+
+        else:
+          print("Please enter a valid amount consisting of no more than 2 numbers after the \".\"\nPlease try again\n")
+          return False
+      else:
+        print("Please enter a valid amount consisting of full euros or with a decimal value seperated by a single \".\"\nPlease try again\n")
+        return False
+    except ValueError:
+      print("Please provide a correct amount consisting of only numbers or numbers with a decimal value seperated by a \".\"\nPlease try again\n")
+      return False
+
+
+if __name__ == "__main__":
+    welcome()
